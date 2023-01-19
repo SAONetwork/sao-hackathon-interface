6c5c3e28654246c90691d3fc0f2dbb6514d4d21b
Matt <matt@sao.network>
2023-01-19 10:46:26 +0800

    fix redirect

--- src/views/Profile/Profile.vue
+++ src/views/Profile/Profile.vue
@@ -431,7 +431,7 @@
 						}
 					});
 				} else {
-					location.href = "https://harmonious-treacle-9cadd3.netlify.app";
+					location.href = "https://demo.storverse.app";
 				}
 			})
 			this.$refs.scrollbar.handleScroll=()=>{
@@ -577,7 +577,7 @@
 			Disconnect() {
 				this.$disconnect();
 
-				location.href = "https://harmonious-treacle-9cadd3.netlify.app";
+				location.href = "https://demo.storverse.app";
 			},
 			getSummary() {
 				getUserSummary()
